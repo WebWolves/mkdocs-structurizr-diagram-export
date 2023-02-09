2c9b1f4965240d87da35d488d0b8fdec0c9005af
Volodymyr Lytvynov <v.v.lytvynov.ua@gmail.com>
2023-02-09 13:14:25 +0200

    Restructurization

--- src/plugin.py
+++ src/plugin.py
@@ -1,29 +1,25 @@
 from mkdocs.plugins import BasePlugin
-from mkdocs.config.base import Config
 from mkdocs.config import config_options as c
 from pathlib import Path
 
 from .utils import spawnProcessSync
 
-class ExportStructurizrDiagramsConfig(Config):
-    """defines the plugin configuration"""
-    # determines workspace file location
-    workspacePath = c.Type(str, default='workspace.dsl')
-
-    # determines export format
-    format = c.Type(str, default='mermaid')
-
-    # determines output location
-    outputPath = c.Type(str, default='docs/diagrams/')
-
-
-class ExportStructurizrDiagrams(BasePlugin[ExportStructurizrDiagramsConfig]):
+class ExportStructurizrDiagrams(BasePlugin):
     """Export Structurizr diagrams to defined format"""
 
-    def export(self, config):
+    config_scheme = (
+        # determines workspace file name
+        ('workspacePath', c.Type(str, default='workspace.dsl')),
+        # determines export format
+        ('format', c.Type(str, default='mermaid')),
+        # determines output location
+        ('outputPath', c.Type(str, default='docs/diagrams/'))
+    )
+
+    def export(self):
         spawnProcessSync('docker pull ghcr.io/aidmax/structurizr-cli-docker')
         spawnProcessSync(f'docker run --rm -v {Path.cwd()}:/root/data -w /root/data ghcr.io/aidmax/structurizr-cli-docker \
             export --workspace {self.config.workspacePath} --format {self.config.format} --output {self.config.outputPath}')
 
-    def on_serve(self, config):
+    def on_serve(self):
         self.export()
